3b723212b44340b1e56701bf0ba078e6f0325f4c
Fareedsea <fareed.sea@gmail.com>
2024-04-18 15:34:36 +0500

    Updated New Word Counting Project Files

---
+++ index.ts
@@ -0,0 +1,11 @@
+
+import inquirer from "inquirer";
+const answers = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Enter any Sentence to count word",
+});
+
+const wordCount = answers.sentence.trim().split(" ");
+
+console.log(`Your Sentence Total Words are ${wordCount.length}`);
